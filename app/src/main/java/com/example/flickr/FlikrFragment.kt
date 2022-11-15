6a11fa268bbf30a18cce13855dabd2c039bd45ec
mohamedsalad <kukgiplayer2@gmail.com>
2022-11-15 14:16:44 -0500

    Added functionality to get Flickr by results including Dialogs, Menus, custom HTTP Requests

--- app/src/main/java/com/example/flickr/FlikrFragment.kt
+++ app/src/main/java/com/example/flickr/FlikrFragment.kt
@@ -1,11 +1,12 @@
 package com.example.flickr
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.util.Log
+import android.view.*
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.lifecycleScope
@@ -18,9 +19,10 @@ import com.example.flickr.databinding.FragmentFlickrBinding
 import kotlinx.coroutines.launch
 
 //import com.squareup.picasso.Picasso;
-class FlikrFragment : Fragment() {
+class FlikrFragment(activity :MainActivity) : Fragment() {
     lateinit var binding : FragmentFlickrBinding
     lateinit var viewModel :FlickerFragmentViewModel
+    val request_code = 0
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -32,22 +34,59 @@ class FlikrFragment : Fragment() {
         binding.apply {
 //            var samples = ArrayList<Photo>()
 //            samples.add(Photo("https://w7.pngwing.com/pngs/929/843/png-transparent-lebron-james-jumping-while-holding-basketball-file-formats-lebron-james-image-file-formats-computer-wallpaper-competition-event-thumbnail.png"))
-            lifecycleScope.launch {
-                viewModel.immutableFlickrStateFlow.collect {
-
-                    var adapter =
-                        FlickrAdapter(it.photos.photo, this@FlikrFragment.requireContext())
-                    rv.layoutManager = GridLayoutManager(this@FlikrFragment.requireContext(), 3)
-                    rv.adapter = adapter
-                    adapter.notifyDataSetChanged()
-                }
-            }
+            executeSearch("")
 
         }
-        lifecycleScope.launch {
-            viewModel.fetchData()
-        }
+
+        setHasOptionsMenu(true)
         return binding.root
+    }
+
+     fun executeSearch(query :String){
+         binding.apply {
+        lifecycleScope.launch {
+            if(query.equals("")){
+                viewModel.fetchData()
+            }
+            else{
+                viewModel.fetchByQuery(query)
+            }
+            viewModel.immutableFlickrStateFlow.collect {
+                var adapter =
+                    FlickrAdapter(it.photos.photo, this@FlikrFragment.requireContext())
+                rv.layoutManager = GridLayoutManager(this@FlikrFragment.requireContext(), 3)
+                rv.adapter = adapter
+                adapter.notifyDataSetChanged()
+            }
+        }
+         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.fragment_flickr_list, menu)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if(resultCode == 0){
+            if(data!!.hasExtra("msg")){
+                var message = data.getStringExtra("msg")
+                Log.d("FlikrFragment","Msg: "+message)
+                executeSearch(message!!)
+            }
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.search_item){
+//            Toast.makeText(requireContext(),"Clicked Search!",Toast.LENGTH_LONG).show()
+            var alertDialog = FlickrDialogFragment()
+            alertDialog.setTargetFragment(this@FlikrFragment,request_code)
+            alertDialog.show(requireFragmentManager(),"TAG")
+
+
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     class FlickrViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
